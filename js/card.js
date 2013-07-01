f8fef4255308a0690e49e0baa6eb97a4e30caaa9
Alma Madsen <madsenat@gmail.com>
2013-07-01 09:18:27 -0600

    print styles for sample deck

--- js/card.js
+++ js/card.js
@@ -1,6 +1,16 @@
 var Card = function(id, data) {
 	this.id = id;
 	this.data = data;
+	var deepClone = function(data) {
+		var ret = _.clone(data);
+		_.each(data, function(val, key) {
+			if (_.isArray(val) || _.isObject(val)) {
+				ret[key] = deepClone(val);
+			}
+		});
+		return ret;
+	};
+	this._initialData = deepClone(data);
 };
 
 Card.prototype.updateData = function(data) {
